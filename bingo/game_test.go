20f3d38c6cd0a757f912491ad71a53c3febc7637
Jacob Patterson <jacob.patterson@gmail.com>
2021-11-09 15:49:38 -0800

    more shared tests

--- bingo/game_test.go
+++ bingo/game_test.go
@@ -7,93 +7,73 @@ import (
 )
 
 func TestGameNumbersLeft(t *testing.T) {
-	for i, test := range gameNumbersLeftTests {
-		if got := test.game.NumbersLeft(); test.want != got {
-			t.Errorf("test %v: wanted %v numbers left, got %v", i, test.want, got)
+	for i, test := range gameTests {
+		if want, got := test.wantNumbersLeft, test.game.NumbersLeft(); want != got {
+			t.Errorf("test %v: wanted %v numbers left, got %v", i, want, got)
 		}
 	}
 }
 
 func TestGameDrawNumber(t *testing.T) {
 	rand.Seed(1257894000) // seed the available numbers for the first test
-	for i, test := range gameDrawNumberTests {
+	for i, test := range gameTests {
 		test.game.DrawNumber()
-		if got := test.game; !reflect.DeepEqual(test.want, got) {
-			t.Errorf("test %v: games not equal after number drawn:\nwanted: %v\ngot:    %v", i, test.want, got)
+		if want, got := test.wantAvailableAfterDraw, test.game; !reflect.DeepEqual(want, got) {
+			t.Errorf("test %v: games not equal after number drawn:\nwanted: %v\ngot:    %v", i, want, got)
 		}
 	}
 }
 
 func TestResetGame(t *testing.T) {
-	g := Game{
-		DrawnNumbers: []Number{23},
-		availableNumbers: []Number{47, 39},
-	}
-	g.Reset()
-	if want, got := 75, len(g.availableNumbers); want != got {
-		t.Errorf("wanted %v available numbers after reset, got %v", want, got)
-	}
-	if want, got := 0, len(g.DrawnNumbers); want != got {
-		t.Errorf("wanted %v drawn numbers after reset, got %v", want, got)
+	for i, test := range gameTests {
+		test.game.Reset()
+		if want, got := 75, len(test.game.availableNumbers); want != got {
+			t.Errorf("test %v: wanted %v available numbers after reset, got %v", i, want, got)
+		}
 	}
 }
 
-var gameNumbersLeftTests = []struct {
-	game Game
-	want int
-}{
-	{},
-	{
-		game: Game{
-			availableNumbers: []Number{1, 2, 3},
-		},
-		want: 3,
-	},
-	{
-		game: Game{
-			DrawnNumbers:     []Number{6, 43, 8},
-			availableNumbers: []Number{19},
-		},
-		want: 1,
-	},
-}
-
-var gameDrawNumberTests = []struct {
-	game Game
-	want Game
+var gameTests = []struct {
+	game                   Game
+	wantAvailableAfterDraw Game
+	wantNumbersLeft        int
 }{
 	{ // shuffle numbers if game is not initialized; random generator is seeded at 1257894000 for these results
-		want: Game{
+		wantAvailableAfterDraw: Game{
 			DrawnNumbers:     []Number{24},
 			availableNumbers: []Number{20, 64, 54, 6, 62, 25, 43, 22, 57, 10, 40, 28, 29, 30, 73, 75, 69, 68, 23, 2, 37, 36, 15, 38, 26, 8, 18, 51, 49, 53, 42, 1, 32, 52, 71, 16, 65, 5, 35, 31, 9, 12, 59, 34, 4, 33, 39, 17, 41, 27, 67, 70, 11, 55, 56, 13, 72, 46, 19, 58, 3, 47, 14, 74, 45, 66, 48, 44, 63, 21, 50, 61, 60, 7},
 		},
+		wantNumbersLeft: 0,
 	},
 	{ // NOOP if the game has drawn numbers and there are no available numbers to draw
 		game: Game{
 			DrawnNumbers: []Number{1},
 		},
-		want: Game{
+		wantAvailableAfterDraw: Game{
 			DrawnNumbers: []Number{1},
 		},
+		wantNumbersLeft: 0,
 	},
 	{ // first draw
 		game: Game{
 			DrawnNumbers:     []Number{},
 			availableNumbers: []Number{8, 14, 3},
 		},
-		want: Game{
+		wantAvailableAfterDraw: Game{
 			DrawnNumbers:     []Number{8},
 			availableNumbers: []Number{14, 3},
 		},
+		wantNumbersLeft: 3,
 	},
 	{ // draw from front of available numbers, add to end of drawn numbers
 		game: Game{
 			DrawnNumbers:     []Number{7, 28, 3},
 			availableNumbers: []Number{21, 34, 18},
 		},
-		want: Game{
+		wantAvailableAfterDraw: Game{
 			DrawnNumbers:     []Number{7, 28, 3, 21},
 			availableNumbers: []Number{34, 18},
 		},
+		wantNumbersLeft: 3,
 	},
 }
