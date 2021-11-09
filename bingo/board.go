3f7cf785609c804ea699dd2fe7ccfb558f5b7b4f
Jacob Patterson <jacob.patterson@gmail.com>
2021-11-09 08:36:10 -0800

    adding board hasLine(), isFilled()

--- bingo/board.go
+++ bingo/board.go
@@ -17,11 +17,80 @@ func NewBoard() *Board {
 			columnRowCounts &^= clearRowCountMask
 			r++
 			if c == 2 && r == 2 {
-				r++ // skip center cell
+				r++ // skip center cell (leave free cell as zero)
 			}
 			setRowCountMask := r << (3 * c)
 			columnRowCounts |= setRowCountMask
 		}
 	}
 	return &b
+}
+
+func (b Board) HasLine(g Game) bool {
+	nums := numberSet(g)
+	for i := 0; i < 5; i++ {
+		if b.hasColumn(i, nums) || b.hasRow(i, nums) {
+			return true
+		}
+	}
+	return b.hasDiagonal1(nums) || b.hasDiagonal2(nums)
+}
+
+func (b Board) IsFilled(g Game) bool {
+	nums := numberSet(g)
+	for _, n := range b {
+		if _, ok := nums[n]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func numberSet(g Game) map[Number]struct{} {
+	s := make(map[Number]struct{}, len(g.DrawnNumbers)+1)
+	s[0] = struct{}{} // free cell
+	for _, n := range g.DrawnNumbers {
+		s[n] = struct{}{}
+	}
+	return s
+}
+
+func (b Board) hasColumn(c int, nums map[Number]struct{}) bool {
+	for r := 0; r < 5; r++ {
+		i := c*5 + r
+		if _, ok := nums[b[i]]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func (b Board) hasRow(r int, nums map[Number]struct{}) bool {
+	for c := 0; c < 5; c++ {
+		i := c*5 + r
+		if _, ok := nums[b[i]]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func (b Board) hasDiagonal1(nums map[Number]struct{}) bool {
+	for j := 0; j < 5; j++ {
+		i := j*5 + j
+		if _, ok := nums[b[i]]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func (b Board) hasDiagonal2(nums map[Number]struct{}) bool {
+	for j := 0; j < 5; j++ {
+		i := j*5 + 4 - j
+		if _, ok := nums[b[i]]; !ok {
+			return false
+		}
+	}
+	return true
 }
--- bingo/board_test.go
+++ bingo/board_test.go
@@ -9,16 +9,147 @@ import (
 func TestNewBoard(t *testing.T) {
 	rand.Seed(1257894001) // seed the available numbers for the first test
 	// &[B 15 B 8 B 4 B 12 B 10 I 19 I 27 I 16 I 28 I 25 N 42 N 41 ? N 31 N 40 G 49 G 52 G 50 G 46 G 57 O 64 O 72 O 67 O 70 O 74]
-	want := &Board{
+	b := Board{
 		15, 8, 4, 12, 10, // B
 		19, 27, 16, 28, 25, // I
 		42, 41, 0, 31, 40, // N
 		49, 52, 50, 46, 57, // G
 		64, 72, 67, 70, 74, // O
 	}
+	want := &b
 	got := NewBoard()
 	if !reflect.DeepEqual(want, got) {
 
 		t.Errorf("boards not equal:\nwanted: %v\ngot:    %v", want, got)
 	}
+}
+
+func TestHasLine(t *testing.T) {
+	b := Board{
+		15, 8, 4, 12, 10, // B
+		19, 27, 16, 28, 25, // I
+		42, 41, 0, 31, 40, // N
+		49, 52, 50, 46, 57, // G
+		64, 72, 67, 70, 74, // O
+	}
+	for i, test := range hasLineTests {
+		g := Game{
+			DrawnNumbers: test.nums,
+		}
+		got := b.HasLine(g)
+		if test.want != got {
+			t.Errorf("test %v (%v): wanted %v, got %v", i, test.name, test.want, got)
+		}
+	}
+}
+
+func TestIsFilled(t *testing.T) {
+	b := Board{
+		15, 8, 4, 12, 10, // B
+		19, 27, 16, 28, 25, // I
+		42, 41, 0, 31, 40, // N
+		49, 52, 50, 46, 57, // G
+		64, 72, 67, 70, 74, // O
+	}
+	t.Run("more-than-line", func(t *testing.T) {
+		for i, test := range hasLineTests {
+			g := Game{
+				DrawnNumbers: test.nums,
+			}
+			if want, got := false, b.IsFilled(g); want != got {
+				t.Errorf("test %v (%v): wanted isFilled() = %v, got %v", i, test.name, want, got)
+			}
+		}
+	})
+	t.Run("same as board", func(t *testing.T) {
+		g := Game{
+			DrawnNumbers: b[:],
+		}
+		if want, got := true, b.IsFilled(g); want != got {
+			t.Errorf("wanted isFilled() = %v, got %v", want, got)
+		}
+	})
+	t.Run("all numbers drawn", func(t *testing.T) {
+		g := Game{
+			DrawnNumbers: []Number{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75},
+		}
+		if want, got := true, b.IsFilled(g); want != got {
+			t.Errorf("wanted isFilled() = %v, got %v", want, got)
+		}
+	})
+}
+
+var hasLineTests = []struct {
+	name string
+	nums []Number
+	want bool
+}{
+	{
+		name: "B column",
+		nums: []Number{15, 8, 4, 12, 10},
+		want: true,
+	},
+	{
+		name: "I column",
+		nums: []Number{19, 27, 16, 28, 25},
+		want: true,
+	},
+	{
+		name: "N column",
+		nums: []Number{42, 41, 31, 40},
+		want: true,
+	},
+	{
+		name: "G column",
+		nums: []Number{49, 52, 50, 46, 57},
+		want: true,
+	},
+	{
+		name: "O column",
+		nums: []Number{64, 72, 67, 70, 74},
+		want: true,
+	},
+	{
+		name: "row 1",
+		nums: []Number{15, 19, 42, 49, 64},
+		want: true,
+	},
+	{
+		name: "row 2",
+		nums: []Number{8, 27, 41, 52, 72},
+		want: true,
+	},
+	{
+		name: "row 3",
+		nums: []Number{4, 16, 50, 67},
+		want: true,
+	},
+	{
+		name: "row 4",
+		nums: []Number{12, 28, 31, 46, 70},
+		want: true,
+	},
+	{
+		name: "row 5",
+		nums: []Number{10, 25, 40, 57, 74},
+		want: true,
+	},
+	{
+		name: "diagonal 1",
+		nums: []Number{15, 27, 46, 74},
+		want: true,
+	},
+	{
+		name: "diagonal 2",
+		nums: []Number{10, 28, 52, 64},
+		want: true,
+	},
+	{
+		name: "clover leaf, no corners",
+		nums: []Number{8, 19, 27, 12, 28, 25, 49, 52, 72, 46, 57, 70},
+		want: false,
+	},
+	{
+		name: "no numbers",
+	},
 }
