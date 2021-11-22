51c2356d5ca2fd60213731a3cff44d2d3799814b
Jacob Patterson <jacob.patterson@gmail.com>
2021-11-22 12:11:53 -0800

    improving gzip handler
    The gzip handler has a ServeHTTP function writes to the provided handler without an anonymous function.

--- server/handler/gzip.go
+++ server/handler/gzip.go
@@ -7,31 +7,40 @@ import (
 	"strings"
 )
 
-// wrappedResponseWriter wraps response writing with another writer.
-type wrappedResponseWriter struct {
-	io.Writer
-	http.ResponseWriter
+type (
+	// gzipHandler wraps a handler to write it with gzipHandler encoding if the request accepts it.
+	gzipHandler struct {
+		http.Handler
+	}
+	// wrappedResponseWriter wraps response writing with another writer.
+	wrappedResponseWriter struct {
+		io.Writer
+		http.ResponseWriter
+	}
+)
+
+// WithGzip wraps the handler with a handler that writes responses using gzip compression when accepted.
+func WithGzip(h http.Handler) http.Handler {
+	return &gzipHandler{h}
+}
+
+// ServeHTTP writes all output with gzip encoding if the request allows it.
+func (h *gzipHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		h.Handler.ServeHTTP(w, r)
+		return
+	}
+	gzw := gzip.NewWriter(w)
+	defer gzw.Close()
+	wrw := wrappedResponseWriter{
+		Writer:         gzw,
+		ResponseWriter: w,
+	}
+	wrw.Header().Set("Content-Encoding", "gzip")
+	h.Handler.ServeHTTP(wrw, r)
 }
 
 // Write delegates the write to the wrapped writer.
 func (wrw wrappedResponseWriter) Write(p []byte) (n int, err error) {
 	return wrw.Writer.Write(p)
-}
-
-// WithGzip wraps the handler with a handler that writes responses using gzip compression when accepted.
-func WithGzip(h http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			h.ServeHTTP(w, r)
-			return
-		}
-		gzw := gzip.NewWriter(w)
-		defer gzw.Close()
-		wrw := wrappedResponseWriter{
-			Writer:         gzw,
-			ResponseWriter: w,
-		}
-		wrw.Header().Set("Content-Encoding", "gzip")
-		h.ServeHTTP(wrw, r)
-	}
 }
