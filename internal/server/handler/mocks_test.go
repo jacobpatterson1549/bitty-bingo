f52ae51f1ade8925f63a21523e2b7c982a123ca9
jacobpatterson1549 <jacob.patterson@gmail.com>
2022-10-05 12:38:08 -0700

    adding Content-Disposition write-ordering test

--- internal/server/handler/mocks_test.go
+++ internal/server/handler/mocks_test.go
@@ -1,6 +1,10 @@
 package handler
 
-import "image"
+import (
+	"bytes"
+	"image"
+	"net/http"
+)
 
 // mockBarcoder always returns the image and error.
 type mockBarcoder struct {
@@ -13,4 +17,29 @@ type mockBarcoder struct {
 func (m *mockBarcoder) Barcode(format string, boardID string, width, height int) (image.Image, error) {
 	m.lastFormat = format
 	return m.Image, m.err
+}
+
+// mockResponseWriter is a very simple http.ResponseWriter.
+// It can track if the header is written before all writes to the body.
+type mockResponseWriter struct {
+	header             http.Header
+	writer             bytes.Buffer
+	statusCode         int
+	headerWrittenFirst bool
+	writeCalled        bool
+}
+
+func (m *mockResponseWriter) Header() http.Header {
+	m.headerWrittenFirst = !m.writeCalled
+	return m.header
+}
+
+func (m *mockResponseWriter) Write(p []byte) (int, error) {
+	m.writeCalled = true
+	return m.writer.Write(p)
+}
+
+func (m *mockResponseWriter) WriteHeader(statusCode int) {
+	m.headerWrittenFirst = !m.writeCalled
+	m.statusCode = statusCode
 }
